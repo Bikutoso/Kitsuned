f4828cf7f46a8d03bb79894832e52e5a3623e506
Victoria Solli <10584311+Bikutoso@users.noreply.github.com>
2025-07-28 18:10:45 +0200

    partally working hand

--- items/pokerhands.lua
+++ items/pokerhands.lua
@@ -1,42 +1,51 @@
 
 SMODS.PokerHand {
-  key = "Cat/Dog",
+  key = 'cat_dog',
   chips = 80,
   mult = 8,
   l_chips = 30,
   l_mult = 4,
   example = {
-    { "S_K", true},
-    { "S_Q", true},
-    { "S_J", true},
-    { "S_9", true},
-    { "S_8", true},
+    { 'S_K', true},
+    { 'C_Q', true},
+    { 'S_J', true},
+    { 'D_9', true},
+    { 'H_8', true}
  },
  evaluate = function(parts, hand)
-  if not next(parts._all_pairs) then
-    local valid_high_low = {
-      ["Big Dog"] = {"Ace", "9"},
-      ["Big Cat"] = {"King", "8"},
-      ["Little Cat"] = {"8", "3"},
-      ["Little Dog"] = {"7", "2"}}
-    for hand_type, high_low in pairs(valid_high_low) do
-      high = false
-      low = false
-      
-      for i, card in ipars(hand) do
-        if i == 1 and card.base.value == high_low[1] then
-          high = true
-        end
-        if i == 5 and card.base.value == high_low[2] then
-          low = true
-        end
-      end
+  if #hand < 5 then return {} end
+  if next(parts._all_pairs) then return {} end
+  
+  local valid_high_low = {
+    ["Big Dog"] = {"Ace", "9"},
+    ["Big Cat"] = {"King", "8"},
+    ["Little Cat"] = {"8", "3"},
+    ["Little Dog"] = {"7", "2"}
+    }
 
-      if high and low then
-        return {hand}
+  for hand_type, high_low in pairs(valid_high_low) do
+    high = nil
+    low = nil
+    
+    for i = 1, #hand do
+      sendDebugMessage("Curhand: "..hand[i].base.value, "Ktsu")
+      if not i == 1 or not i == 5 then
+        local current_hand = hand[i].base.value
+        if current_hand == high_low[1] then
+          sendDebugMessage("Hightrue: "..hand[i].base.value, "Ktsu")
+          high = current_hand
+        end
+        if current_hand == high_low[2] then
+          sendDebugMessage("Lowtrue: "..hand[i].base.value, "Ktsu")
+          low = current_hand
+        end
       end
     end
+
+    if high and low then
+      return {hand}
+    end
+
   end
-  return {}
- end,
+ end
 }
---
+++ localization/default.lua
@@ -0,0 +1,14 @@
+return {
+    misc = {
+        poker_hands={
+          ["ktsu_cat_dog"] = "Cat/Dog",
+        },
+        poker_hand_descriptions={
+          ["ktsu_cat_dog"] = {
+            "From a set high to set low",
+            "with non-pair cards inbetween",
+            "(Valid high/low: A-9, K-8, 8-7, 7-2)",
+          }
+        }
+    }
+}
