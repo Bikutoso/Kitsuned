218691f9697b61ce8893c2d7389281ed9c80df1e
Victoria Solli <10584311+Bikutoso@users.noreply.github.com>
2025-07-30 04:30:49 +0200

    Moved cat/dog to table indexing instead of iteratively finding the hand

--- items/utilities.lua
+++ items/utilities.lua
@@ -3,46 +3,32 @@ function calculate_cat_dog(hand)
   local fox_present = false
   if next(SMODS.find_card("j_ktsu_fox")) then fox_present = true end
 
-  -- NOTE: Maybe more efficent to instead of itterating through every hand
-  --  high_low high card from table then low to find hand played.
-  --  Would also remove make all fox hands the same removing need for checking
   local poker_hands = {
-    ["Big Dog"] = {"Ace", "9"},
-    ["Big Cat"] = {"King", "8"},
-    ["Little Cat"] = {"8", "3"},
-    ["Little Dog"] = {"7", "2"}
+    ["Ace"] = {["9"] = "Big Dog"},
+    ["King"] = {["8"] = "Big Cat"},
+    ["Queen"] = {["7"] = "Fox"},
+    ["Jack"] = {["6"] = "Fox"},
+    ["10"] = {["5"] = "Fox"},
+    ["9"] = {["4"] = "Fox"},
+    ["8"] = {["3"] = "Little Cat"},
+    ["7"] = {["2"] = "Little Dog"}
   }
-  local poker_hands_fox = {
-    ["Fox Queen"] = {"Queen", "7"},
-    ["Fox Jack"] = {"Jack", "6"},
-    ["Fox Ten"] = {"10", "5"},
-    ["Fox Nine"] = {"9", "4"}
-  }
-
   -- Find higest and lowest played card
-  local high_low = {0, 12}
+  local high_low = {{0, nil}, {12, nil}}
   for _, card in ipairs(hand) do
     local current_hand = SMODS.Ranks[card.base.value].pos.x
-    if high_low[1] < current_hand then high_low[1] = current_hand end
-    if high_low[2] > current_hand then high_low[2] = current_hand end
+    if high_low[1][1] < current_hand then high_low[1] = {current_hand, card.base.value} end
+    if high_low[2][1] > current_hand then high_low[2] = {current_hand, card.base.value} end
   end
 
-  local calculate = function(valid_hands, high_low, fox)
-    -- Check if valid hand
-    for hand_type, current_hand in pairs(valid_hands) do
-      local high = nil
-      local low = nil
-
-      if high_low[1] == SMODS.Ranks[current_hand[1]].pos.x then high = current_hand[1] end
-      if high_low[2] == SMODS.Ranks[current_hand[2]].pos.x then low = current_hand[2] end
-
-      if high and low then
-        if fox then return "Fox" end --If neihter a Cat or Dog it's a fox
-        return hand_type
-      end
-    end
-    return nil
+  sendDebugMessage("High/Low: "..inspectDepth(high_low), "Ktsu")
+ 
+  -- Find Valid Hand
+  local high = high_low[1][2]
+  local low = high_low[2][2]
+  if poker_hands[high] then
+    local played_hand = poker_hands[high][low]
+    if played_hand == "Fox" and not fox_present then return {} end -- If no fox don't fox
+    return played_hand
   end
-
-  return nil or calculate(poker_hands, high_low) or (fox_present and calculate(poker_hands_fox, high_low, true))
 end
