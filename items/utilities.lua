5b133483a5fd1cc59c88e2839090c75896f073fc
Victoria Solli <10584311+Bikutoso@users.noreply.github.com>
2025-07-30 02:49:06 +0200

    Removed eyesore of a lookuptable

--- items/utilities.lua
+++ items/utilities.lua
@@ -1,32 +1,27 @@
 function calculate_cat_dog(hand)
 
-   local fox_present = false
-   if next(SMODS.find_card("j_ktsu_fox")) then fox_present = true end
-  -- Table probbably easier way of doing this, just don't know how
-  local card_order = {
-    ["Ace"] = 14, ["King"] = 13, ["Queen"] = 12, ["Jack"] = 11,
-    ["10"] = 10, ["9"] = 9, ["8"] = 8, ["7"] = 7, ["6"] = 6,
-    ["5"] = 5, ["4"] = 4, ["3"] = 3, ["2"] = 2
-    }
+  local fox_present = false
+  if next(SMODS.find_card("j_ktsu_fox")) then fox_present = true end
 
   local valid_high_low = {
-    ["Big Dog"] = {"Ace", "9"},
-    ["Big Cat"] = {"King", "8"},
-    ["Little Cat"] = {"8", "3"},
-    ["Little Dog"] = {"7", "2"}
+    ["Big Dog"] = {12, 7},   -- Ace-9
+    ["Big Cat"] = {11, 6},   -- King-8
+    ["Little Cat"] = {6, 1}, -- 8-3
+    ["Little Dog"] = {5, 0}  -- 7-2
   }
   local fox_high_low = {
-    ["Fox Queen"] = {"Queen", "7"},
-    ["Fox Jack"] = {"Jack", "6"},
-    ["Fox Ten"] = {"10", "5"},
-    ["Fox Nine"] = {"9", "4"}
+    ["Fox Queen"] = {10, 5}, -- Queen-7
+    ["Fox Jack"] = {9, 4},   -- Jack-6
+    ["Fox Ten"] = {8, 3},    -- 10-5
+    ["Fox Nine"] = {7, 2}    -- 9-4
   }
   
   local calculate = function(valid_hands, fox)
     --sendDebugMessage("Entered Func: "..(fox and "true" or "false"), "Ktsu")
     local select = {0, 14}
+    local current_hand = nil
     for _, card in ipairs(hand) do
-      local current_hand = card_order[card.base.value]
+      current_hand = SMODS.Ranks[card.base.value].pos.x
       if select[1] < current_hand then select[1] = current_hand end
       if select[2] > current_hand then select[2] = current_hand end
     end
@@ -35,8 +30,8 @@ function calculate_cat_dog(hand)
       local high = nil
       local low = nil
 
-      if select[1] == card_order[high_low[1]] then high = high_low[1] end
-      if select[2] == card_order[high_low[2]] then low = high_low[2] end
+      if select[1] == high_low[1] then high = high_low[1] end
+      if select[2] == high_low[2] then low = high_low[2] end
 
       if high and low then
         --sendDebugMessage("Valid hand: "..hand_type, "Ktsi")
