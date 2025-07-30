feeadb3124e89f58a072c6c1700b4c58b3d2b2ed
Victoria Solli <10584311+Bikutoso@users.noreply.github.com>
2025-07-30 03:27:24 +0200

    better variable naming in cat/dog pokerhand

--- items/utilities.lua
+++ items/utilities.lua
@@ -3,13 +3,16 @@ function calculate_cat_dog(hand)
   local fox_present = false
   if next(SMODS.find_card("j_ktsu_fox")) then fox_present = true end
 
-  local valid_high_low = {
+  -- NOTE: Maybe more efficent to instead of itterating through every hand
+  --  high_low high card from table then low to find hand played.
+  --  Would also remove make all fox hands the same removing need for checking
+  local poker_hands = {
     ["Big Dog"] = {"Ace", "9"},
     ["Big Cat"] = {"King", "8"},
     ["Little Cat"] = {"8", "3"},
     ["Little Dog"] = {"7", "2"}
   }
-  local fox_high_low = {
+  local poker_hands_fox = {
     ["Fox Queen"] = {"Queen", "7"},
     ["Fox Jack"] = {"Jack", "6"},
     ["Fox Ten"] = {"10", "5"},
@@ -17,21 +20,21 @@ function calculate_cat_dog(hand)
   }
 
   -- Find higest and lowest played card
-  local select = {0, 12}
+  local high_low = {0, 12}
   for _, card in ipairs(hand) do
     local current_hand = SMODS.Ranks[card.base.value].pos.x
-    if select[1] < current_hand then select[1] = current_hand end
-    if select[2] > current_hand then select[2] = current_hand end
+    if high_low[1] < current_hand then high_low[1] = current_hand end
+    if high_low[2] > current_hand then high_low[2] = current_hand end
   end
 
   local calculate = function(valid_hands, high_low, fox)
     -- Check if valid hand
-    for hand_type, poker_hand in pairs(valid_hands) do
+    for hand_type, current_hand in pairs(valid_hands) do
       local high = nil
       local low = nil
 
-      if select[1] == SMODS.Ranks[poker_hand[1]].pos.x then high = poker_hand[1] end
-      if select[2] == SMODS.Ranks[poker_hand[2]].pos.x then low = poker_hand[2] end
+      if high_low[1] == SMODS.Ranks[current_hand[1]].pos.x then high = current_hand[1] end
+      if high_low[2] == SMODS.Ranks[current_hand[2]].pos.x then low = current_hand[2] end
 
       if high and low then
         if fox then return "Fox" end --If neihter a Cat or Dog it's a fox
@@ -41,5 +44,5 @@ function calculate_cat_dog(hand)
     return nil
   end
 
-  return nil or calculate(valid_high_low, select) or (fox_present and calculate(fox_high_low, select, true))
+  return nil or calculate(poker_hands, high_low) or (fox_present and calculate(poker_hands_fox, high_low, true))
 end
