30a69096aa83476635765a585282a1660564571b
Victoria Solli <10584311+Bikutoso@users.noreply.github.com>
2025-08-01 22:31:03 +0200

    Have no idea what do now

--- items/pokerhands.lua
+++ items/pokerhands.lua
@@ -1,26 +1,51 @@
 
 SMODS.PokerHand {
   key = 'cat_dog',
-  chips = 30,
-  mult = 4,
-  l_chips = 30,
-  l_mult = 2,
+  chips = 40,
+  mult = 5,
+  l_chips = 15,
+  l_mult = 3,
   above_hand = "Straight",
   example = {
     {"S_K", true},
-    {"C_Q", true},
-    {"S_J", true},
-    {"D_9", true},
+    {"C_Q", false},
+    {"S_J", false},
+    {"D_9", false},
     {"H_8", true}
   },
   evaluate = function(parts, hand)
     if #hand < 5 then return {} end
-    ---BUG: Deleting Shortcut while having a cat/dog selected still have Straight hand selected
     if next(parts._all_pairs) or next(parts._straight) then return {} end
 
-    return calculate_cat_dog(hand) and {hand} or {}
+    high = next(parts._highest)
+    low = next(parts.ktsu_lowest)
+
+    sendDebugMessage("High: "..inspect(high).."\nLow: "..inspect(low), "ktsu")
+    
+    return { SMODS.merge_lists(parts._highest, parts.ktsu_lowest) }
+    --return calculate_cat_dog(hand, false)
   end,
   modify_display_text = function(self, cards, scoring_hand)
-    return calculate_cat_dog(scoring_hand)
+    return "cat/dog"
+    --return calculate_cat_dog(scoring_hand, true)
+  end
+}
+
+SMODS.PokerHandPart {
+  key = "lowest",
+  func = function(hand)
+    if #hand < 1 then return {} end
+    local low_card = {}
+    local low_raw = 11
+    
+    for _, card in ipairs(hand) do
+      local card_value = card.base.value
+      if SMODS.Ranks[card_value].pos.x < low_raw then
+        low_card[1] = card
+        low_raw = SMODS.Ranks[card_value].pos.x
+      end
+    end
+    --sendDebugMessage("Part: "..inspectDepth(low_card), "ktsu")
+    return low_card
   end
 }
--- items/utilities.lua
+++ items/utilities.lua
@@ -1,4 +1,4 @@
-function calculate_cat_dog(hand)
+function calculate_cat_dog(hand, display)
   local fox_present = false
   if next(SMODS.find_card("j_ktsu_fox")) then fox_present = true end
 
@@ -14,21 +14,32 @@ function calculate_cat_dog(hand)
     ["7"] = {["2"] = "Little Dog"}
   }
   -- Find higest and lowest played card
-  local high_low = {["high"]= "2", ["low"] = "Ace"}
-  for _, card in ipairs(hand) do
+  local high_low_raw = {["high"] = "2", ["low"] = "Ace"}
+  local high_low = {}
+  for i, card in ipairs(hand) do
+    -- Note but we do want stone if kicker?
     if SMODS.has_no_suit(card) then return {} end -- Not valid hand if stone
 
     local current_card = card.base.value
     local current_card_int = SMODS.Ranks[current_card].pos.x
-    if SMODS.Ranks[high_low["high"]].pos.x < current_card_int then high_low["high"] = current_card end
-    if SMODS.Ranks[high_low["low"]].pos.x > current_card_int then high_low["low"] = current_card end
-  end
-
+    if SMODS.Ranks[high_low_raw["high"]].pos.x < current_card_int then
+      high_low_raw["high"] = current_card
+      high_low[i] = card
+    elseif SMODS.Ranks[high_low_raw["low"]].pos.x > current_card_int then
+      high_low_raw["low"] = current_card
+      high_low[i] = card
+    else
+      high_low[i] = {}
+    end
+  end    
   -- Find Valid Hand
-  if poker_hands[high_low["high"]] then
-    local played_hand = poker_hands[high_low["high"]][high_low["low"]]
-    
+  if poker_hands[high_low_raw["high"]] then
+    local played_hand = poker_hands[high_low_raw["high"]][high_low_raw["low"]]
+
+    if display then return played_hand or {} end -- Display Text
+
+    if not played_hand then return {} end -- Non Valid hand 
     if played_hand == "Fox" and not fox_present then return {} end -- If no fox don't fox
-    return played_hand
+    return high_low
   end
 end
